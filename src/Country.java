b36a140eb5925c3a70e43be6b7ca3d5f9ebc42e5
SweetSnowyWitch <snowysatan@yandex.ru>
2021-12-25 16:06:34 +0500

    Create DB

--- src/Country.java
+++ src/Country.java
@@ -1,19 +1,19 @@
 public class Country {
-    private final String country;
-    private final String region;
-    private final Integer happinessRank;
-    private final Float happinessScore;
-    private final Float standardError;
-    private final Float economy;
-    private final Float family;
-    private final Float health;
-    private final Float freedom;
-    private final Float trust;
-    private final Float generosity;
-    private final Float dystopiaResidual;
+    public String country;
+    public String region;
+    public Integer happinessRank;
+    public Float happinessScore;
+    public Float standardError;
+    public Float economy;
+    public Float family;
+    public Float health;
+    public Float freedom;
+    public Float trust;
+    public Float generosity;
+    public Float dystopiaResidual;
 
     public Country(String country, String region, Integer happinessRank, Float happinessScore,
-                   Float standardError,Float economy, Float family, Float health, Float freedom,
+                   Float standardError, Float economy, Float family, Float health, Float freedom,
                    Float trust, Float generosity, Float dystopiaResidual) {
         this.country = country;
         this.region = region;
@@ -29,51 +29,9 @@ public class Country {
         this.dystopiaResidual = dystopiaResidual;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public Integer getHappinessRank() {
-        return happinessRank;
-    }
-
-    public Float getHappinessScore() {
-        return happinessScore;
-    }
-
-    public Float getStandardError() {
-        return standardError;
-    }
-
-    public Float getEconomy() {
-        return economy;
-    }
-
-    public Float getFamily() {
-        return family;
-    }
-
-    public Float getHealth() {
-        return health;
-    }
-
-    public Float getFreedom() {
-        return freedom;
-    }
-
-    public Float getTrust() {
-        return trust;
-    }
-
-    public Float getGenerosity() {
-        return generosity;
-    }
-
-    public Float getDystopiaResidual() {
-        return dystopiaResidual;
+    @Override
+    public String toString() {
+        return country + " " + region + " " + happinessRank + " " + standardError + " " + economy +
+                " " + family + " " + health + " " + freedom + " " + trust + " " + generosity + " " + dystopiaResidual;
     }
 }
